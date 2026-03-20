5dac223e60c370a222a3dcb18a7328cc5d2b6b25
Nguyễn Trường Vinh Sơn <ntvs1707@gmail.com>
2026-03-20 16:02:37 +0700

    Up Code

---
+++ src/main/java/com/example/vinhson2761/Vinhson2761Application.java
@@ -0,0 +1,13 @@
+package com.example.vinhson2761;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vinhson2761Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vinhson2761Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vinhson2761/Vinhson2761ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinhson2761;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vinhson2761ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
